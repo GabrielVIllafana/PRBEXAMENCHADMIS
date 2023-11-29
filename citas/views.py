fc80d3ea4c7147810321833220eb029bb3448073
AramisBolton <asanchezm2@toluca.tecnm.mx>
2023-11-28 18:31:06 -0600

    Funciones completas sin estilos

--- citas/views.py
+++ citas/views.py
@@ -9,8 +9,7 @@ def index(request):
     mascotas = Mascotas.objects.all()
     medicos = Medicos.objects.all()
     if request.method == 'POST':
-        # print('Fecha: '+request.POST['fecha']+' Hora: '+request.POST['hora']+' Mascota_id: '+request.POST['mascota']+' Medico_id: '+request.POST['medico'])
-        Citas.objects.create(fecha=request.POST['fecha'], hora=request.POST['hora'], mascota_id=request.POST['mascota'], medico_id=request.POST['medico'])
+       Citas.objects.create(fecha=request.POST['fecha'], hora=request.POST['hora'], mascota_id=request.POST['mascota'], medico_id=request.POST['medico'])
     return render(request, 'index.html', {
         'title': 'Ajendar Cita',
         'form': crearCita,
@@ -19,6 +18,14 @@ def index(request):
     })
 
 def citas(request):
+    mascotas = Mascotas.objects.all()
+    medicos = Medicos.objects.all()
+    citas = Citas.objects.all()
+    duenios = Duenios.objects.all()
     return render(request, 'citas.html', {
-        'title': 'Citas Ajendadas'
+        'title': 'Citas Ajendadas',
+        'mascotas': mascotas,
+        'medicos': medicos,
+        'citas': citas,
+        'duenios': duenios
     })
